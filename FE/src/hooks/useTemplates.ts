e687f91fb9d66d1d144b47eb76cec7eab91a3893
RaffiRmdhn <klsterbuka@gmail.com>
2026-05-10 00:19:50 +0700

    chore: project configuration, dependencies update, and cleanup

--- FE/src/hooks/useTemplates.ts
+++
@@ -1,9 +0,0 @@
-import { templateService } from '@/services/template.service';
-import { useQuery } from '@tanstack/react-query';
-
-export const useTemplates = () => {
-  return useQuery({
-    queryKey: ['templates'],
-    queryFn: templateService.getAll,
-  });
-};
